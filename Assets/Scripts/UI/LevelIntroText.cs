0388b106311ebd278a7467763fd2397abf91f61a
evann delaunay <evann.delaunay@univ-lyon2.fr>
2021-12-07 16:50:16 +0100

    -changed text hide animation -added glitch text

--- Assets/Scripts/UI/LevelIntroText.cs
+++ Assets/Scripts/UI/LevelIntroText.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Random = UnityEngine.Random;
 
 
 public class LevelIntroText : MonoBehaviour
@@ -39,13 +40,29 @@ public class LevelIntroText : MonoBehaviour
         float incTimer = 0;
         while (incTimer<2)
         {
-            distanceText = "\n"+(int) Mathf.Lerp(0, distance, incTimer / 2.0f) + "KM";
+            if(distance == 0)
+                distanceText = "\n"+Random.Range(0,1000000000) + "KM";
+            else
+                distanceText = "\n" + (int)Mathf.Lerp(0, distance, incTimer / 2.0f) + "KM";
             incTimer += Time.deltaTime;
             textDisplay.text = text+distanceText;
             yield return null;
         }
-        distanceText = "\n" + distance + "KM";
+        if(distance == 0)
+            distanceText = "\n" + "100000000000000000000000000000000000000" + "KM";
+        else
+            distanceText = "\n" + distance + "KM";
         textDisplay.text = text + distanceText;
+
+        text += distanceText;
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        for (int i = text.Length-1; i >= 0; i--)
+        {
+            textDisplay.text = text.Substring(0,i);
+            yield return new WaitForSecondsRealtime(0.02f);
+        }
 
         yield return new WaitForSecondsRealtime(2f);
         textDisplay.enabled = false;
