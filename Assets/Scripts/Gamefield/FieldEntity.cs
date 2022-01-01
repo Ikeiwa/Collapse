32f63b6e7547775eb4dbe7440c80c81cafab75e8
CANDELA Nicolas <darkxell.mc@gmail.com>
2022-01-01 19:18:44 +0100

    Entity instances

--- Assets/Scripts/Gamefield/FieldEntity.cs
+++ Assets/Scripts/Gamefield/FieldEntity.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FieldEntity : MonoBehaviour
 {
+
+    public Gamefield parent;
+
     void Start()
     {
         
@@ -11,10 +14,12 @@ public class FieldEntity : MonoBehaviour
 
     void FixedUpdate()
     {
-        
+        // TODO : remove this, abstract Field entities have no AI
+        gameObject.transform.position = gameObject.transform.position + (Vector3.right * 0.1f);
     }
 
     public bool IntersectsPlayer() {
         return false;
+        // TODO: compute player intersection for everything.
     }
 }
--- Assets/Scripts/Gamefield/Gamefield.cs
+++ Assets/Scripts/Gamefield/Gamefield.cs
@@ -9,16 +9,21 @@ using UnityEngine;
 public class Gamefield : MonoBehaviour
 {
     [SerializeField]
+    public GameObject PREFAB_Enemy_Basic;
+
+    [SerializeField]
     public GameObject anchorBackLeft;
     [SerializeField]
     public GameObject anchorBackRight;
 
+    public readonly List<GameObject> content = new List<GameObject>(200);
+
     void Start()
     {
-        
+        Instantiate(PREFAB_Enemy_Basic, anchorBackLeft.transform.position, Quaternion.identity, this.transform);
     }
 
-    void Update()
+    void FixedUpdate()
     {
         
     }
