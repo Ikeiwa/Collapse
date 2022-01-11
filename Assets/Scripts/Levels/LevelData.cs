5ea27923635f5e88d7d7aed62888ea6d87e4f63f
CANDELA Nicolas <darkxell.mc@gmail.com>
2022-01-11 14:45:20 +0100

    Documented LevelData

--- Assets/Scripts/Levels/LevelData.cs
+++ Assets/Scripts/Levels/LevelData.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Level", menuName = "Collapse/Level", order = 1)]
 public class LevelData : ScriptableObject
 {
+    /// <summary>
+    /// String displaying the name of the level in selection and transitionnal UIs
+    /// </summary>
     public string displayName = "Level";
+    /// <summary>
+    /// int displaying the conceptual distance left to the singularity. Used for display purposes or ordering
+    /// </summary>
     public int distance = 1000;
+    /// <summary>
+    /// Duration of the level, in seconds
+    /// </summary>
     public float duration = 120;
+    /// <summary>
+    /// Background track playing in the 
+    /// </summary>
     public AudioClip music;
+    /// <summary>
+    /// Display 
+    /// </summary>
     public Color skyColor;
+    /// <summary>
+    /// Maximum level curvature. Only used for display purposes.
+    /// </summary>
     public Vector3 maxCurve;
 
     public LevelTile startTile;
