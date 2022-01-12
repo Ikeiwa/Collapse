d8a1cedf1e13a643b989837eeb66da357e73fb68
CANDELA Nicolas <darkxell.mc@gmail.com>
2022-01-12 16:42:50 +0100

    Update Level1.cs

--- Assets/Scripts/Levels/Content/bundles/Level1.cs
+++ Assets/Scripts/Levels/Content/bundles/Level1.cs
@@ -15,12 +15,12 @@ public class Level1 : LevelContent
         // First creep spawn
         content.Add(new SpawnTestEnemy(2f));
         // Obstacle stairs
-        content.Add(new RObstacle2(5f));
-        content.Add(new RObstacle3(5.5f));
         content.Add(new RObstacle1(6f));
-        content.Add(new RObstacle4(6.5f));
+        content.Add(new RObstacle2(6.5f));
+        content.Add(new RObstacle3(7f));
+        content.Add(new RObstacle4(7.5f));
         // Next level
-        content.Add(new NextLevelTrigger(8f));
+        content.Add(new NextLevelTrigger(9f));
     }
 
 }
