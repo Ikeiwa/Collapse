eb541202c7170af908effc76ebb953677f5a4584
CANDELA Nicolas <darkxell.mc@gmail.com>
2022-01-20 16:45:48 +0100

    Added stage 1 midboss

--- Assets/Scripts/Gamefield/Enemies/MagmaDiver.cs
+++ Assets/Scripts/Gamefield/Enemies/MagmaDiver.cs
@@ -7,6 +7,11 @@ public class MagmaDiver : AbstractEnemy
 
     private Vector3 startposition;
 
+    private static readonly Vector3[] wings = new Vector3[] { new Vector3(-2*4, 0, 2), new Vector3(-3*4, 0, 2.5f), new Vector3(-4*4, 0, 3), new Vector3(-5*4, 0, 3),
+        new Vector3(-6*4, 0, 3), new Vector3(-7*4, 0, 3), new Vector3(-8*4, 0, 2.5f), new Vector3(-9*4, 0, 2),
+        new Vector3(2*4, 0, 2), new Vector3(3*4, 0, 2.5f), new Vector3(4*4, 0, 3), new Vector3(5*4, 0, 3),
+        new Vector3(6*4, 0, 3), new Vector3(7*4, 0, 3), new Vector3(8*4, 0, 2.5f), new Vector3(9*4, 0, 2)};
+
     private int frameloop = 0;
 
     void Start()
@@ -14,11 +19,22 @@ public class MagmaDiver : AbstractEnemy
         startposition = new Vector3(transform.position.x, 0, transform.position.z);
     }
 
+    private float angle = 0f;
+    private int shotcooldown = 3;
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
-
-        Vector3 newposition = startposition + new Vector3(10, 0, 0);
+        angle += 0.03f;
+        Vector3 newposition = BulletMathUtils.getCirclePos(startposition, 10, angle);
+        transform.position = new Vector3(newposition.x, transform.position.y, newposition.z);
+        Debug.Log("Health : " + health);
+        shotcooldown--;
+        if (shotcooldown <= 0 && Grounded()) for (int i = 0; i < wings.Length; i++)
+            {
+                shotcooldown = 3;
+                Vector3 mispos = transform.position + wings[i];
+                gf.AddProjectile(gf.PREFAB_Shot_LinearSmall, mispos, AbstractBullet.QUATERNION_DOWN, new BulletArguments { speed = 1.6f });
+            }
     }
 
     public override void OnKill()
